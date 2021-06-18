d41149fd6e2451012007f2b5d4d22fb535809f2f
Samuel Dupret <samu.dupret@hotmail.com>
2021-06-18 17:13:19 +0100

    clean start for new empirica create app

--- server/randomFunctions.js
+++
@@ -1,6 +0,0 @@
-//Function to randomly choose an element from an array:
-export const choice = (array) => {
-    var randomIndex = Math.floor(Math.random() * array.length);
-    var randomElement = array[randomIndex];
-    return randomElement;
-}
